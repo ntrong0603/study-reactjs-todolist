5b1fe1c262a415f0945129c0b4001f8ac84997fe
Le Ngoc Trong <ntrong0603.dgt@gmail.com>
2019-04-17 08:16:16 +0700

    css list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
-
+// Immutability khong co kha nang thay doi trang thai ben trong
+//"mutate thay doi trang thai ben trong"
 class App extends Component {
   constructor(){
     super();
-    this.todoItems = [
-      { title: 'Mua bin bin', isComplete: true }, 
-      { title: 'An' , isComplete: false}, 
-      { title: 'uong' , isComplete: false}
-    ]
+    this.state ={ 
+      todoItems:  [
+          { title: 'Mua bin bin', isComplete: true }, 
+          { title: 'An' , isComplete: false}, 
+          { title: 'uong' , isComplete: false}
+        ]
+    }
   }
+  onItemClick(item){
+    return () => {
+      const isComplete = item.isComplete;
+      const index = this.state.todoItems.indexOf(item);
+      const todoItems = this.state.todoItems;
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index), 
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)]
+      });
+    }
+  }
+  //truyen 1 function thong qua props
   render() {
+    const {todoItems} = this.state;
     return (
       <div className="App">
+          <div className="Title">
+            <h2>Todo List</h2>
+          </div>
           {
-            this.todoItems.map((item, index) => <TodoItem key = {index} item={item} />)
+            todoItems.length > 0 && todoItems.map(
+              (item, index) => 
+              <TodoItem 
+                key = {index} 
+                item={item} 
+                onClick ={this.onItemClick(item)}
+              />
+            )
           }
+          {todoItems.length === 0 && 'Nothing here...'}
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,22 +2,47 @@
 // import react
 // component co the nhan vao nhieu props
 import React, { Component } from 'react';
+
+// them thu vien classnames de xu ly su kioen len quan den class css
+import classnames from 'classnames';
+
 import './TodoItem.css';
+import checkImg from "./images/check.svg";
+import checkImg_done from "./images/check-complete.svg";
 
 //khai bao class
+// trong react props la read only
 class TodoItem extends Component{
-    //method render reuturn ra element muon su dung
+    // method render reuturn ra element muon su dung
+    // // demo su ly su kien trong react
+    // constructor(props){
+    //     super(props);
+
+    //     this.onItemClick = this.onItemClick.bind(this);
+    // }
+
+    // onItemClick(){
+        
+    // }
+
     render() {
-        const item = this.props.item;
-        /*
-        hoac dung const {item} = this.props;
-        */
-        let className = "to-Do-Item";
+        const {item, onClick} = this.props;
+        // /*
+        // hoac dung const {item} = this.props;
+        // */
+        // let className = "to-Do-Item";
+        // if(item.isComplete){
+        //     className += " to-Do-Item-Done";
+        // }
+        let url = checkImg;
         if(item.isComplete){
-            className += " to-Do-Item-Done";
+            url = checkImg_done;
         }
         return (
-            <div className = {className}>
+            // cach sua loi truyen vao mot ham goi ham onItemClick
+            // neu truyen this.onItemClick() vao thay vi this.onItemClick thi ham se thuc thi ngay
+            <div  className = {classnames( 'to-Do-Item', {'to-Do-Item-Done': item.isComplete})}>
+                <img onClick = {onClick} src = {url} alt = {item.title} />
                 <p> {item.title}</p>
             </div>
         );
